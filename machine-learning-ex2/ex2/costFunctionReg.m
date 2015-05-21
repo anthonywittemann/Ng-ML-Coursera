8b276edfd394fb85494c26eccab1cd7868f04859
anthonywittemann <awittema@usc.edu>
2015-05-20 23:19:41 -0700

    successfully implemented regularization for logistic regression

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -18,7 +18,45 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+totalCost = 0;
+n = size(theta, 1);
+for i = 1:m
+    regCost = 0;
+    for j=2:n
+      regCost = regCost + theta(j)^2;
+    endfor 
 
+    totalCost = totalCost + ((-y(i)*log(sigmoid((X(i,:) * theta))) -
+    ((1-y(i))*log(1-sigmoid((X(i,:) * theta))))) + lambda/(2*m) * regCost);
+end 
+
+% fprintf("Total Cost: %f \n", totalCost); 
+
+J = 1 / m * totalCost;
+
+fprintf("J: %f \n", J);
+
+
+temp = theta;
+
+totalCost1 = 0;
+for i = 1:m
+    totalCost1 = totalCost1 + (sigmoid(X(i,:) * temp) - y(i))*X(i,1);
+endfor    
+grad(1) = 1 / m * totalCost1;
+
+    
+for j=2:size(X,2)
+  totalCost = 0;
+  for i = 1:m
+    totalCost = totalCost + (sigmoid(X(i,:) * temp) - y(i))*X(i,j) ...
+                + lambda / m * temp(j);
+  endfor
+      
+  grad(j) = 1 / m * totalCost;
+endfor
+
+fprintf("Gradient: %f \n", grad);
 
 
 
